904fb631ab6ded4a583e31905aa5a3c2e76da6fd
Venkateswara Reddy Marri <venkateswarareddy@nbos.vinisoft.com>
2021-10-20 14:25:37 +0530

    second commit

--- src/main/java/com/example/demo/DemoSpringApplication.java
+++ src/main/java/com/example/demo/DemoSpringApplication.java
@@ -8,6 +8,8 @@ public class DemoSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringApplication.class, args);
+		
+		System.out.println("welcome to git hub !");
 	}
 
 }
